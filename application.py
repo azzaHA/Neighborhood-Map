19a9d6bcf1db859b6b60f2210305f8f07564fc7a
Azza-Ahmed <azza.h.mostafa@gmail.com>
2018-06-30 20:39:16 -0700

    Complete project

--- application.py
+++ application.py
@@ -9,18 +9,13 @@ import random
 @app.route('/')
 def index():
     """Render home page"""
-    #return search('baskin robins', 37.3879874, -122.0889856)
     return render_template('index.html')
 
 @app.route('/search/<string:term>/<string:lat>/<string:lng>')
 def search(term, lat, lng):
     key = '4Ra9ADtqXDLUhbebEnA3uccwMcidVYRkAXE-uf1oYIoFdc1LaZGEB3Mb-AoFBmuspGbs4V_hQuX7d3P-zVUykCmsuiBe3qxdbypXzgAwqG8S4Ym6dziaeNX70RM1W3Yx'
-    #term = 'baskin robins'
-    #lat = 37.3879874
-    #lng = -122.0889856
     lat = float(lat)
     lng = float(lng)
-    #location = '700 Agnew Rd, Santa Clara'
     response = ''
     try:
         response = query_api(term, lat, lng, key)
@@ -33,9 +28,6 @@ def search(term, lat, lng):
             )
         )
     return jsonify(response)
-
-
-
 
 if __name__ == '__main__':
  app.debug = True
--- static/app.js
+++ static/app.js
@@ -1,4 +1,17 @@
-var places = [
+// Get the map from google api the first thing, in an async request
+$.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAhP2DMRVyLDfq_Ewe9U8q9PD0_mMpKj60&v=3&callback=initMap",
+function() {
+   console.log("Script loaded");
+});
+
+// Public variables to be used across the code
+let map;
+let markers = [];
+let currentMarker;
+let currentInfoWindow;
+
+// names and locations of the places to be displayed
+let places = [
   {
     name: 'Baskin-Robbins',
     location: {lat: 37.3879874, lng: -122.0889856}
@@ -29,78 +42,124 @@ var places = [
   },
 ];
 
+// Knockout place observable, representing the Model
 let Place = function(data){
   this.name = ko.observable(data.name);
   this.location = ko.observable(data.location);
+  this.isVisible = ko.observable(data.isVisible);
 }
 
+// Knockout ViewModel, containing the logic of viewing the places in list view,
+// and filtering them
 let ViewModel = function(){
   let self = this;
+
+  // Initialize observable array of places
   this.placesList = ko.observableArray([]);
   places.forEach(function(place){
+    place.isVisible = true;
     self.placesList.push(new Place(place));
   });
 
   this.currentPlace = ko.observable(this.placesList()[0]);
 
-  
+  // the current place is set when the user clicks on an item in the list
   this.setCurrentPlace = function(place){
     self.currentPlace(place);
-    console.log(place.name);
+    let placeIndex = self.placesList().indexOf(place);
+    if (currentMarker){
+      currentMarker.setAnimation(null);
+    }
+    currentMarker = markers[placeIndex];
+    currentMarker.setAnimation(google.maps.Animation.BOUNCE);
+    getYelpData();
   }
+
+  // the filter query entered by the user, binded to the filter text box in the view
+  this.filterQuery = ko.observable('');
+
+  // Filter function, to be called whenever the user updates the value inside the filter box
+  this.filterLocations = function(){
+    self.placesList().forEach(function(place){
+      let placeName = place.name().toLowerCase();
+      let query = self.filterQuery().toLowerCase();
+      let placeIndex = self.placesList().indexOf(place);
+
+      // check if the entered filter query is a substring of the place name
+      if (placeName.includes(query)){
+        // isVisible property controls whether the list item should be shown or hidden
+        place.isVisible(true);
+        // setting the map makes the marker visible on it
+        markers[placeIndex].setMap(map);
+      }
+      else{
+        place.isVisible(false);
+        markers[placeIndex].setMap(null);
+      }
+    });
+  }
+
+  // filterLocations is called whenever the value inside filterQuery changes
+  this.filterQuery.subscribe(function(){
+    self.filterLocations();
+  });
 
 }
 
 ko.applyBindings(new ViewModel());
 
-
-$.getScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAhP2DMRVyLDfq_Ewe9U8q9PD0_mMpKj60&v=3&callback=initMap",
-function() {
-   console.log("Script loaded");
-});
-
-
-var map;
-var markers = [];
+// initialize the map along with the markers and their event listeners
 function initMap() {
+
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.386052, lng: -122.083851},
     zoom: 15
   });
-  var defaultIcon = makeMarkerIcon('0091ff');
+  let defaultIcon = makeMarkerIcon('0091ff');
 
-  var largeInfowindow = new google.maps.InfoWindow();
+  let largeInfowindow = new google.maps.InfoWindow();
+  currentInfoWindow = largeInfowindow;
   // The following group uses the location array to create an array of markers on initialize.
-  for (var i = 0; i < places.length; i++) {
+  for (let i = 0; i < places.length; i++) {
     // Get the position from the location array.
-    var position = places[i].location;
-    var title = places[i].name;
+    let position = places[i].location;
+    let title = places[i].name;
     // Create a marker per location, and put into markers array.
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
       position: position,
       title: title,
       animation: google.maps.Animation.DROP,
       icon: defaultIcon,
       id: i
     });
-    // Push the marker to our array of markers.
+    // Push the marker to array of markers.
     marker.setMap(map);
     markers.push(marker);
     // Create an onclick event to open the large infowindow at each marker.
     marker.addListener('click', function() {
-      console.log('inside listener');
-      let url = '/search/' + this.title + '/' + this.position.lat() + '/' + this.position.lng();
-      var currentMarker = this;
-      var window = largeInfowindow;
-      $.getJSON(url, function (response){
-        console.log(response);
-        populateInfoWindow(currentMarker, response, window);
-      }).error(function(e){
-        alert(e);
-      });
+
+      if (currentMarker){
+        currentMarker.setAnimation(null);
+      }
+      currentMarker = this;
+      getYelpData();
     });
   }
+}
+
+// Get data from yelp, by sending an async request to the server with current place parameters
+function getYelpData(){
+  let url = '/search/' + currentMarker.title + '/' + currentMarker.position.lat() + '/' + currentMarker.position.lng();
+  $.getJSON(url, function (response){
+    // if the request is handled successfully, animate the current marker and
+    // fill the currentInfoWindow with additional place information
+    currentMarker.setAnimation(google.maps.Animation.BOUNCE);
+    populateInfoWindow(currentMarker, response, currentInfoWindow);
+  }).error(function(e){
+    // in case of an error, alert the user
+    alert('Error, Data not loaded.\nDetails: Status Code: ' +e.status + ', Message: ' +  e.statusText);
+  });
 }
 
 function populateInfoWindow(marker, response, infowindow) {
@@ -127,21 +186,22 @@ function populateInfoWindow(marker, response, infowindow) {
       infowindow.open(map, marker);
       // Make sure the marker property is cleared if the infowindow is closed.
       infowindow.addListener('closeclick', function() {
+        currentMarker.setAnimation(null);
         infowindow.marker = null;
       });
     }
   }
 
-  // This function takes in a COLOR, and then creates a new marker
-        // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-        // of 0, 0 and be anchored at 10, 34).
-        function makeMarkerIcon(markerColor) {
-          var markerImage = new google.maps.MarkerImage(
-            'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-            '|40|_|%E2%80%A2',
-            new google.maps.Size(21, 34),
-            new google.maps.Point(0, 0),
-            new google.maps.Point(10, 34),
-            new google.maps.Size(21,34));
-          return markerImage;
-        }
+// This function takes in a COLOR, and then creates a new marker
+// icon of that color. The icon will be 21 px wide by 34 high, have an origin
+// of 0, 0 and be anchored at 10, 34).
+function makeMarkerIcon(markerColor) {
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21,34));
+  return markerImage;
+}
